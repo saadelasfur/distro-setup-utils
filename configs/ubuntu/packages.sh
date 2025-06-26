5575b1bae344dd3fa3245712d34453ba97bcb24b
Saad <saadelasfur@gmail.com>
2025-06-26 16:13:22 +0300

    feat: add distro support (ubuntu)
    Signed-off-by: Saad <saadelasfur@gmail.com>

---
+++ configs/ubuntu/packages.sh
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2025 saadelasfur
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Distro packages list
+
+PACKAGES=()
+PACKAGES+=(
+    "aapt" "adduser" "attr" "bc" "bison"
+    "binutils" "brotli" "busybox" "build-essential" "ccache"
+    "clang" "cmake" "cpio" "coreutils" "curl"
+    "ffmpeg" "file" "flex" "g++" "git"
+    "git-lfs" "gnupg" "golang" "gperf" "imagemagick"
+    "jq" "libbrotli-dev" "libelf-dev" "libgtest-dev" "liblz4-dev"
+    "liblz4-tool" "libncurses-dev" "libpcre2-dev" "libprotobuf-dev" "libsdl1.2-dev"
+    "libssl-dev" "libxml2" "libxml2-utils" "libzstd-dev" "lld"
+    "lz4" "lzop" "neofetch" "nodejs" "npm"
+    "openjdk-17-jdk" "p7zip-full" "proot" "protobuf-compiler" "python3"
+    "python3-dev" "python-is-python3" "rsync" "schedtool" "squashfs-tools"
+    "swig" "tar" "tree" "webp" "wget"
+    "xsltproc" "xxd" "zip" "zipalign" "zlib1g-dev"
+)
